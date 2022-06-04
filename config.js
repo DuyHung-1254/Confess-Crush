1d31eafdaace5415d6bdbf8bad1d3486058f33db
Nguyen Vu Duy Hung <94880378+DuyHung-1254@users.noreply.github.com>
2022-06-04 13:52:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Chị Hà Iu Dáu",
+    introTitle: 'Phạm Nguyệt Hà',
+    introDesc: `Hà Iu Dáu - Mốt Cho Iem Về Nhà Chị Ăn Cơm Nghen - 爱妹妹 `,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Oke Iem Iu <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
