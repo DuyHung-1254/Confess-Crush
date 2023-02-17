96cc23c62d5771e5fc8092a81b637442967bc9a1
Nguyen Vu Duy Hung <94880378+DuyHung-1254@users.noreply.github.com>
2023-02-17 20:58:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng Anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Anh có thấy bé Thảo dễ thương hong',
-    btnYes: 'Cóa,Gất Iu Iem <33',
+    btnYes: 'Cóa,Gất Dễ Thương, Iu Iem <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Anh lại yêu Em <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 10h, mình qua đón đi chơi nha, Quẩy bar bay lắc các kiểu nha.',
+    mess: 'Biết mà 🥰. Iu Anh 😘😘',
+    messDesc: 'Tối nay 10h, Bé qua đón đi chơi nha, Quẩy bar bay lắc các kiểu nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
